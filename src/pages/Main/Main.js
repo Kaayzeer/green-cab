8d105f2a07c5abcd548e9d411064169db47b20cb
nikolas <nikolas.rosinelli@atudent.kyh.se>
2021-11-11 20:01:57 +0100

    changed the route in landing to exact path

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -22,7 +22,7 @@ function Main() {
         <Route path="/search">
           <Search />
         </Route>
-        <Route path="/">
+        <Route exact path="/">
           <Landing />
         </Route>
       </Switch>
